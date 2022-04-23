ac2bf00dc085fe0e531d66eb938dd3ff9839d22f
mkolp11597753 <18670319384@163.com>
2022-04-23 17:39:20 +0800

    删除多余代码

--- loaders/md-loader.js
+++ loaders/md-loader.js
@@ -1,22 +1,5 @@
 const marked = require('marked')
 
-const escapeHtml = (html) =>
-  html
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#x27;")
-
-const ff = (str) => 
-  str
-  .replace(/&amp;/g, "&")
-  .replace(/&lt;/g, "<")
-  .replace(/&gt;/g, ">")
-  .replace(/&quot;/g, '"')
-  .replace(/&#x27;/g, "'")
-
-
 module.exports = function MdLoader(source) {
   const html = marked.parse(source)
   let code = JSON.stringify(html) // Invalid in JavaScript but valid HTML
